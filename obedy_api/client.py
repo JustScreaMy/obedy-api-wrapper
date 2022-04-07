8d36f79155dd5284db943b097ce2ec02839e1406
Jakub Kropáček <kropikuba@gmail.com>
2022-04-07 22:23:04 +0200

    IT WORKS!

--- obedy_api/client.py
+++ obedy_api/client.py
@@ -1,4 +1,5 @@
 import requests
+from obedy_api.models import Food
 from obedy_api.parser import Parser
 from obedy_api.exceptions import InvalidPageTypeException
 
@@ -11,17 +12,18 @@ class APIClient:
     _password: str
 
     _logged: bool = False
-    _paser: Parser
 
     _pages: dict = {
         'food': 'faces/secured/month.jsp',
         'burza': 'faces/secured/burza.jsp',
         'platby': 'faces/secured/platby.jsp',
         'objednavky': 'faces/secured/objednavky.jsp',
-        'historie': 'faces/secured/historie.jsp'
+        'historie': 'faces/secured/historie.jsp',
+        'secured': 'faces/secured/',
     }
 
     def __init__(self, url: str, username: str, password: str):
+
         self._session = requests.Session()
         if not url.endswith('/'):
             url += '/'
@@ -29,10 +31,32 @@ class APIClient:
         self._username = username
         self._password = password
 
+    # TODO: reuse parser?
+    def _get_parser(self, html: str):
+        return Parser(html)
+
+    def order(self, food: Food):
+
+        res = self._session.get(
+            self._base_url + self._pages['secured'] + food.api_uri)
+
+        print(res.text)
+
+        return
+
     def get_food(self):
-        res = self._session.get(self._base_url + self._pages['food'])
-        self._paser = Parser(res.text)
-        return self._paser.parse_food()
+
+        page = self._get_page('food')
+        parser = self._get_parser(page)
+
+        return parser.parse_food()
+
+    def get_credit(self):
+
+        page = self._get_page('food')
+        parser = self._get_parser(page)
+
+        return parser.parse_credit()
 
     @property
     def _login_url(self):
@@ -43,7 +67,8 @@ class APIClient:
         return self._base_url + "logout"
 
     def login(self):
-        token = self.get_token()
+
+        token = self._get_token()
         res = self._session.post(self._login_url, data={
             'targetUrl': 'faces/secured/main.jsp',
             'j_username': self._username,
@@ -51,20 +76,33 @@ class APIClient:
             '_csrf': token,
         })
         self._logged = True
+
         return res.url
 
     def logout(self):
-        token = self.get_token()
+
+        token = self._get_token()
         res = self._session.post(self._login_url, data={
             '_csrf': token,
         })
         self._logged = False
+
         return res.url
 
-    def get_token(self) -> str:
+    def _get_token(self) -> str:
+
         self._session.get(self._base_url)
+
         return self._session.cookies['XSRF-TOKEN']
 
+    def _get_page(self, page: str):
+
+        if page not in self._pages:
+            raise InvalidPageTypeException
+
+        return self._session.get(self._base_url + self._pages[page]).text
+
     def __del__(self):
+
         if self._logged:
             self.logout()
--- obedy_api/parser.py
+++ obedy_api/parser.py
@@ -1,38 +1,52 @@
 from typing import List
 import bs4
 import re
-from obedy_api.models import DayMenu, Food, MonthMenu
+
+from obedy_api.models import Day, Food
 
 DATE_RE = '(\d{2}.\d{2}.\d{4})'
 ORDER_URI_RE = '\'(.*?)\''
 
 
 class Parser():
+    # TODO: Make it reusable?
 
     _soup: bs4.BeautifulSoup
 
     def __init__(self, content: str):
         self._soup = bs4.BeautifulSoup(content, 'html.parser')
 
-    def _parse_day(self, date: str, foods: bs4.ResultSet) -> DayMenu:
-        parsed = []
+    def _parse_day(self, foods: bs4.ResultSet, date: str) -> List[Food]:
+        fl = []
         for food in foods:
             name = food.find(
                 'div', class_='jidWrapCenter').contents[0].lstrip('\n').strip()
             api_uri = re.findall(ORDER_URI_RE, food.find(
                 'a', class_='btn')['onclick'])[0]
-            parsed.append(Food(name, api_uri))
-        return DayMenu(date, parsed)
+            price = float(food.find('span', class_='important').string[:-3])
+            ordered = not not food.find('span', class_='button-link-tick')
+            orderable = not not not food.find('a', class_='disabled')
+            fl.append(Food(name, api_uri, price, ordered, orderable))
+
+        return Day(date, fl)
 
     def parse(self):
         pass
 
-    def parse_food(self) -> MonthMenu:
+    def parse_credit(self) -> dict:
+        cr = self._soup.find('span', id='Kredit').string[:-3].replace(',', '.')
+        return {
+            'credit': float(cr)
+        }
+
+    def parse_food(self) -> List[Day]:
         parsed_days = []
+
         for day in self._soup.find(
                 id='mainContext').find('table').find_all('tr'):
             date = re.findall(DATE_RE, day.select_one(
                 'div.jidelnicekTop').string)[0]
             foods = day.find_all('div', class_='jidelnicekItemWrapper')
-            parsed_days.append(self._parse_day(date, foods))
-        return MonthMenu(parsed_days)
+            parsed_days.append(self._parse_day(foods, date))
+
+        return parsed_days
